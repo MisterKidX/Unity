6c5244e42e7ea740fbea05c84bd09b6606638727
Dor Ben Dor <agesonera@gmail.com>
2020-02-10 15:29:18 +0200

    Added some old yet cool shit

---
+++ Utilities/UI/DraggablePanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggablePanel : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+
+    #region Properties
+
+    Vector2 mouseOffset;
+    Vector3 newPosition;
+    RectTransform parent;
+
+    #endregion
+
+    #region Unity Locals
+
+    void Awake () 
+	{
+        parent = transform.parent as RectTransform;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+    #endregion
+
+    #region Methods
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(transform.parent as RectTransform, eventData.position, eventData.pressEventCamera, out newPosition);
+        parent.position = newPosition - (Vector3)mouseOffset;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        parent.SetAsLastSibling();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)transform.parent, eventData.position, eventData.pressEventCamera, out mouseOffset);
+    }
+
+    #endregion
+}
---
+++ Utilities/UI/ResizeablePanel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ResizeablePanel : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+
+    #region Properties
+
+    RectTransform _parent;
+    Vector2 _currentPointerPosition;
+    Vector2 _lastPointerPosition;
+
+
+    #endregion
+
+    #region Unity Locals
+
+    void Awake () 
+	{
+        _parent = transform.parent as RectTransform;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+    #endregion
+
+    #region Methods
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, eventData.position, eventData.pressEventCamera, out _currentPointerPosition);
+        var currentSize = _parent.sizeDelta;
+
+        _parent.sizeDelta += new Vector2(_currentPointerPosition.x - _lastPointerPosition.x, -_currentPointerPosition.y + _lastPointerPosition.y);
+
+        _lastPointerPosition = _currentPointerPosition;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _parent.SetAsLastSibling();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_parent, eventData.position, eventData.pressEventCamera, out _lastPointerPosition);
+    }
+
+    #endregion
+}

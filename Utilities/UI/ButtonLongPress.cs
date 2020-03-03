1f2585ee721c66f49cec073b6af993da909eaa3c
Dor Ben Dor <agesonera@gmail.com>
2020-03-03 15:20:34 +0200

    Added some new cool stuff
    * Button long press which changes the fill amount of a button based on the click hold

---
+++ Utilities/UI/ButtonLongPress.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonLongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    #region Fields & Autoprops
+
+    public UnityEvent OnHoldClick;
+    public float holdTime;
+
+    private float counter = 0;
+    private bool toCalc = false;
+    private Button btn;
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+    }
+
+    private void OnHoldEventHandler()
+    {
+        if (OnHoldClick != null)
+            OnHoldClick.Invoke();
+    }
+
+    private IEnumerator Measure()
+    {
+        while (true)
+        {
+            counter += Time.deltaTime;
+            btn.image.fillAmount = 1 - (counter / holdTime);
+
+            if (counter < holdTime && toCalc == true)
+                yield return null;
+            else if (toCalc == true && counter > holdTime)
+            {
+                OnHoldEventHandler();
+                break;
+            }
+            else
+                break;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        toCalc = true;
+        StartCoroutine(Measure());
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        toCalc = false;
+        btn.image.fillAmount = 1;
+
+        if (counter >= holdTime)
+            OnHoldEventHandler();
+
+        counter = 0;
+    }
+
+    #endregion
+}

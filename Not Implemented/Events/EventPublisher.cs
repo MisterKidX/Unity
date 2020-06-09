1ba64ce0eb1ef09577853e0ee929ad4fbd56e63b
Dor Ben Dor <agesonera@gmail.com>
2020-06-09 17:20:38 +0300

    added event plaxeholders with SO

---
+++ Not Implemented/Events/EventPublisher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities.Events
+{
+    [CreateAssetMenu(menuName = "Events/Event Publisher")]
+    public class EventPublisher : ScriptableObject
+    {
+        protected List<EventListener> _listeners = new List<EventListener>();
+
+        public void Raise()
+        {
+            for (int i = _listeners.Count - 1; i >= 0; i--)
+                _listeners[i].EventRaisedHandler();
+        }
+
+        public virtual void RegisterListener(EventListener listener)
+        { _listeners.Add(listener); }
+
+        public void UnregisterListener(EventListener listener)
+        { _listeners.Remove(listener); }
+
+    }
+
+    #region Extensions
+
+    //[CreateAssetMenu(menuName = "Events/Flow Event Publisher")]
+    //public class FlowEventPublisher : EventPublisher
+    //{
+        //public override void RegisterListener(EventListener listener)
+        //{
+        //    base.RegisterListener(listener);
+        //}
+        //public void RaiseFlowEvent()
+        //{
+        //    for (int i = _listeners.Count - 1; i >= 0; i--)
+        //        _listeners[i].EventRaisedHandler();
+        //}
+    //}
+
+    #endregion
+}

8d4a4fc51a4a85e7fc297417a47c8fb12ae6dd5b
Dor Ben Dor <agesonera@gmail.com>
2020-06-15 12:55:50 +0300

    Add files via upload

---
+++ Not Implemented/DeactivateOnAwake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class DeactivateOnAwake : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Not Implemented/Fader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Fader : MonoBehaviour
+{
+    Image _image;
+
+    #region Interface
+
+    [SerializeField] float _fadeTime;
+    [SerializeField] UnityEvent FadeComplete = new UnityEvent();
+
+    public void FadeOut()
+    {
+        _image.enabled = true;
+        _image.color = Color.clear;
+        _image.CrossFadeColor(Color.black, _fadeTime, true, true);
+        StartCoroutine(OnFadeComplete(_fadeTime, false));
+    }
+
+    public void FadeIn()
+    {
+        _image.enabled = true;
+        _image.color = Color.black;
+        _image.CrossFadeColor(Color.clear, _fadeTime, true, true);
+        StartCoroutine(OnFadeComplete(_fadeTime, true));
+    }
+
+    #endregion
+
+    #region Implementation
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+        _image.enabled = false;
+    }
+
+    IEnumerator OnFadeComplete(float waitTime, bool deactivate = true)
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+
+        if (FadeComplete != null)
+        {
+            FadeComplete.Invoke();
+        }
+
+        _image.enabled = deactivate ? false : true;
+    }
+
+    #endregion
+
+}
